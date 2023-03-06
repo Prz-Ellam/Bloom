a5d66a2006f4457921e915c912c4ed1bce8bfeca
Prz-Ellam <PerezAlex088@outlook.com>
2023-03-06 17:46:46 -0600

    Database Facade

--- src/Database/DB.php
+++ src/Database/DB.php
@@ -5,11 +5,31 @@ namespace Bloom\Database;
 use Bloom\Application;
 
 class DB {
-    public static function executeNonQuery(string $query, array $parameters) {
-        Application::app()->getDatabaseDriver()->executeNonQuery($query, $parameters);
+    public static function beginTransaction(): bool {
+        return Application::app()->getDatabaseDriver()->beginTransaction();
     }
 
-    public static function executeReader(string $query, array $parameters) {
-        Application::app()->getDatabaseDriver()->executeReader($query, $parameters);
+    public static function inTransaction(): bool {
+        return Application::app()->getDatabaseDriver()->inTransaction();
+    }
+
+    public static function commit(): bool {
+        return Application::app()->getDatabaseDriver()->commit();
+    }
+
+    public static function rollBack(): bool {
+        return Application::app()->getDatabaseDriver()->rollBack();
+    }
+
+    public static function executeNonQuery(string $query, array $parameters): int {
+        return Application::app()->getDatabaseDriver()->executeNonQuery($query, $parameters);
+    }
+
+    public static function executeReader(string $query, array $parameters): array {
+        return Application::app()->getDatabaseDriver()->executeReader($query, $parameters);
+    }
+
+    public static function lastInsertId(): string {
+        return Application::app()->getDatabaseDriver()->lastInsertId();
     }
 }
