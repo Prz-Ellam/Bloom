3015a46deeeaafce2823165118660b4f07bdcac3
Prz-Ellam <PerezAlex088@outlook.com>
2023-03-05 23:54:46 -0600

    Views root

--- src/Application.php
+++ src/Application.php
@@ -51,7 +51,7 @@ class Application {
         $this->response = new Response();
         $this->responseEmitter = new ResponseEmitter();
 
-        $this->templateEngine = new TemplateEngine(dirname(self::$root, 1));
+        $this->templateEngine = new TemplateEngine(self::$root . "/views");
 
         $this->databaseDriver = new PDODatabaseDriver();
         $this->databaseDriver->connect(
