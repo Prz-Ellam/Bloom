b7e32ed0ba73d144946459cb5bdbbe3249386363
Prz-Ellam <PerezAlex088@outlook.com>
2023-03-06 23:24:14 -0600

    EqualTo Rule

---
+++ src/Validations/Rules/EqualTo.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Bloom\Validations\Rules;
+
+use Bloom\Validations\ValidationRule;
+
+class EqualTo extends ValidationRule {
+    private string $fieldName;
+
+    public function __construct(string $fieldName, ?string $message = null) {
+        $this->fieldName = $fieldName;
+        $this->message = $message ?? "El campo no coincide con $fieldName";
+    }
+
+    public function isValid(string $field, array $inputs): bool {
+        return $inputs[$field] === $inputs[$this->fieldName];
+    }
+}
