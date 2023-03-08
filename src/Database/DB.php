1159484585f7d19e190e70c01e6c8869e73f8a19
Prz-Ellam <PerezAlex088@outlook.com>
2023-03-08 15:06:52 -0600

    Fetch one facade

--- src/Database/DB.php
+++ src/Database/DB.php
@@ -25,6 +25,10 @@ class DB {
         return Application::app()->getDatabaseDriver()->executeNonQuery($query, $parameters);
     }
 
+    public static function executeOneReader(string $query, array $parameters): int {
+        return Application::app()->getDatabaseDriver()->executeOneReader($query, $parameters);
+    }
+
     public static function executeReader(string $query, array $parameters): array {
         return Application::app()->getDatabaseDriver()->executeReader($query, $parameters);
     }
