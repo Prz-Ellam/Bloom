b8b441a81323ef7436b14c2d433752e028fd5c01
Prz-Ellam <PerezAlex088@outlook.com>
2023-05-14 17:38:30 -0600

    Update Model

--- src/Database/Model.php
+++ src/Database/Model.php
@@ -2,6 +2,41 @@
 
 namespace Bloom\Database;
 
-class Model {
+use Cursotopia\ValueObjects\EntityState;
+use JsonSerializable;
 
+abstract class Model implements JsonSerializable {
+    protected EntityState $entityState;
+    protected array $_ignores = [];
+
+    public abstract function save(): bool;
+
+    public function jsonSerialize(): mixed {
+        $properties = get_object_vars($this);
+        $output = [];
+        
+        foreach ($properties as $name => $value) {
+            if (in_array($name, $this->_ignores)) {
+                 continue;
+            }
+
+            if ($name == '_ignores') {
+                continue;
+            }
+
+            if (!($value instanceof Repository) && !($value instanceof EntityState)) {
+                $output[$name] = $value;
+            }
+        }
+        
+        return $output;
+    }
+
+    public function toArray(): ?array {
+        return json_decode(json_encode($this), true);
+    }
+
+    public function setIgnores(array $ignores) {
+        $this->_ignores = $ignores;
+    }
 }
